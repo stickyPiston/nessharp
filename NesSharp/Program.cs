2969ed042a55ea8c270f4aae29fd53fbc13b9fbd
Max van Gent <m.e.vangent@students.uu.nl>
2021-11-29 10:23:23 +0100

    Init2

---
+++ NesSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NesSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
