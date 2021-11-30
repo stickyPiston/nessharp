c9e9bbd398e16ca1f3951bdad821d1ddf573c51a
Job Vonk <sticcpistt@icloud.com>
2021-11-30 16:08:53 +0100

    Added unit tests for the bus

--- NesSharp/Bus.cs
+++ NesSharp/Bus.cs
@@ -1,19 +1,40 @@
-﻿using System;
+﻿using System.Threading;
 using System.Collections.Generic;
-using System.Text;
 
-namespace NesSharp
-{
-    class Bus : IAddressable
-    {
-        public byte Read(ushort addr)
-        {
+namespace NesSharp {
+    struct Range {
+        public uint start;
+        public uint end;
+
+        public Range(uint start, uint end) {
+            this.start = start;
+            this.end = end;
+        }
+    };
+
+    class Bus : IAddressable {
+        private List<IAddressable> chips = new();
+        private Dictionary<Range, IAddressable> ranges = new();
+
+        public Bus() {
             throw new NotImplementedException();
         }
 
-        public void Write(ushort addr, byte data)
-        {
+        //public run(string romFilepath) { when the rom accepts roms
+        public run() {
             throw new NotImplementedException();
         }
-    }
+
+        public Register(IAddressable chip, Range[] ranges) {
+            throw new NotImplementedException();
+        }
+
+        public byte Read(ushort addr) {
+            throw new NotImplementedException();
+        }
+
+        public void Write(ushort addr, byte data) {
+            throw new NotImplementedException();
+        }
+    };
 }
---
+++ NesSharpTests/BusTests.cs
@@ -0,0 +1,60 @@
+using NUnit.Framework;
+using NesSharp;
+
+namespace NesSharpTests {
+    class ChipA {
+        private Bus bus;
+        public ChipA(Bus bus) { this.bus = bus; }
+
+        public void exec() {
+            bus.Write(0x120, 0x10);   
+        }
+
+        public byte Read(short addr) {
+            return (addr == 0x12) ? 0x20 : 0x00;
+        }
+        public void Write(short _, byte __) { }
+    }
+
+    class ChipB : NesSharp.IAddressable {
+        private Bus bus;
+        public ChipB(Bus bus) { this.bus = bus; }
+
+        public byte  data = 0;
+        public short addr = 0;
+        public void Write(short addr, byte data) {
+            this.data = data;
+            this.addr = addr;
+        }
+
+        public byte exec() { return bus.Read(0x12); }
+
+        public byte Read(short _) { }
+    }
+
+    public class Tests {
+        ChipA a;
+        ChipB b;
+
+        [SetUp]
+        public void Setup() {
+            var bus = new Bus();
+            a = new ChipA(bus); b = new ChipB(bus);
+            bus.Register(a, new Range(0, 0x100));
+            bus.Register(b, new Range(0x101, 0x200));
+        }
+
+        [Test]
+        public void Test1() {
+            a.exec();
+            Assert.AreEqual(b.sata, 0x10);
+            Assert.AreEqual(b.addr, 0x120);
+        }
+
+        [Test]
+        public void Test2() {
+            byte val = b.exec();
+            Assert.AreEqual(val, 0x20);
+        }
+    }
+}
