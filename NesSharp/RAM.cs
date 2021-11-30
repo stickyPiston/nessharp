c24e240029ee80d2ef7936aa154a4611e2b59e3f
Sem de Haan <s.f.dehaan1@students.uu.nl>
2021-11-30 14:46:47 +0100

    RAM Class

---
+++ NesSharp/RAM.cs
@@ -0,0 +1,17 @@
+﻿namespace NesSharp
+{
+	public class RAM : IAddressable
+	{
+        private byte[] Data = new byte [2048];
+
+        public byte Read(ushort addr)
+        {
+            return Data[addr];
+        }
+
+        public void Write(ushort addr, byte data)
+        {
+            Data[addr] = data;
+        }
+    }
+}
