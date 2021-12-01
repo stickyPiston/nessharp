2cf8aa3b9d1434c29599e35522ab34cfb4e605e0
Lennard Snapper <l.a.snapper@students.uu.nl>
2021-12-01 11:02:28 +0100

    added subclasses and made arrays for ram and rom

--- NesSharp/BaseMapper.cs
+++ NesSharp/BaseMapper.cs
@@ -4,7 +4,30 @@ using System.Text;
 
 namespace NesSharp
 {
-    class BaseMapper
+    abstract class BaseMapper
     {
+        private byte[] prgROM;
+        private byte[] prgRAM = new byte[0x200];
+        private byte[] chrROM;
+    }
+
+    class NRom : BaseMapper
+    {
+
+    }
+
+    class UxRom : BaseMapper
+    {
+
+    }
+    
+    class MMC1 : BaseMapper
+    {
+
+    }
+
+    class MMC3 : BaseMapper
+    {
+
     }
 }
