cceaadf7425e39bb2d55e01736671f45c656627c
Sem de Haan <s.f.dehaan1@students.uu.nl>
2021-12-02 21:53:58 +0100

    Implemented Controller, Controllerport and InputDevice

--- NesSharpTests/ControllerPortTests.cs
+++ NesSharpTests/ControllerPortTests.cs
@@ -1,55 +1,64 @@
 using NUint.Framework;
 
-namespace NesSharpTests {
-  class EmulatedKeyboard : InputDevice {
-    public void latchInput() {
-      // SFML would get input from the keyboard here
-      // Instead, we return a fixed sequence of bits
-      register = 0b10000101 // A, Down, Right
-    }
+namespace NesSharpTests
+{
+    class EmulatedKeyboard : InputDevice
+    {
+        public void latchInput()
+        {
+            // SFML would get input from the keyboard here
+            // Instead, we return a fixed sequence of bits
+            register = 0b10000101; // A, Down, Right
+        }
 
-    public void Read() {
-      byte bit = register & 1;
-      ror(ref register);
-      return bit;
-    }
+        public void Read()
+        {
+            byte bit = register & 1;
+            ror(ref register);
+            return bit;
+        }
+    };
 
-    public byte ror(ref byte b) => (b >> 1) | ((b & 1) << 7);
-  };
+    public class ControllerPortTests
+    {
+        private ControllerPort cp = new ControllerPort();
 
-  public class ControllerPortTests {
-    private ControllerPort cp = new ControllerPort();
+        [SetUp]
+        public void Setup() => cp.register(new EmulatedKeyboard());
 
-    [SetUp]
-    public void Setup() => cp.register(new EmulatedKeyboard());
+        [Test(Description = "Test whether a single controller works")]
+        public void singleControllerTest()
+        {
+            for (uint addr = 0x4016; addr <= 0x4017; addr++)
+            {
+                // We haven't allowed the controller to gather input so
+                // bit should be 0 (the default value) here.
 
-    [Test(Description = "Test whether a single controller works")]
-    public void singleControllerTest() {
-      for (uint addr = 0x4016; addr <= 0x4017; addr++) {
-        // We haven't allowed the controller to gather input so
-        // bit should be 0 (the default value) here.
-        var bit = cp.Read(addr);
-        Assert.AreEqual(bit, 0x0);
+                if (addr == 0x4016)
+                {
+                    var bit = cp.Read(addr);
+                    Assert.AreEqual(bit, 0x0);
+                }
 
-        cp.Write(addr, 0x1); // Allow the gathering of input
-        cp.Write(addr, 0x0); // Stop the gathering of input
+                cp.Write(addr, 0x1); // Allow the gathering of input
+                cp.Write(addr, 0x0); // Stop the gathering of input
 
-        byte output = 0x0;
-        for (uint i = 0; i < 8; i++)
-          output = (output << 1) | cp.Read(addr);
+                byte output = 0x0;
+                for (uint i = 0; i < 8; i++)
+                    output = (output << 1) | cp.Read(addr);
 
-        Assert.AreEqual(output, 0b10000101);
+                Assert.AreEqual(output, 0b10000101);
 
-        bit = cp.Read(addr);
-        Assert.AreEqual(output & 1, bit);
+                bit = cp.Read(addr);
+                Assert.AreEqual(output & 1, bit);
 
-        // The register should reset after the 1->0 writing
-        cp.Write(addr, 0x1); // Allow the gathering of input
-        cp.Write(addr, 0x0); // Stop the gathering of input
+                // The register should reset after the 1->0 writing
+                cp.Write(addr, 0x1); // Allow the gathering of input
+                cp.Write(addr, 0x0); // Stop the gathering of input
 
-        bit = cp.Read(addr);
-        Assert.AreEqual(output & 1, bit);
-      }
-    }
-  };
+                bit = cp.Read(addr);
+                Assert.AreEqual(output & 1, bit);
+            }
+        }
+    };
 };
