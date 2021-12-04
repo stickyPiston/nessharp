76525a8f4c3c1792da8763edee1cbe2be3d82828
Steven Hu <y.h.hu1@students.uu.nl>
2021-12-04 16:09:26 +0100

    added oscpulse

--- NesSharp/APU/sweeper.cs
+++ NesSharp/APU/sweeper.cs
@@ -3,7 +3,6 @@ using System;
 namespace NesSharp
 {
     
-
     class Sweeper
     {
         public bool enabled = true;
@@ -72,4 +71,38 @@ namespace NesSharp
         }
     }
 
+    class oscpulse
+    {
+        double frequency = 0;
+        double dutycycle = 0;
+        double amplitude = 1;
+        double pi = 3.14159;
+        double harmonics = 20;
+
+        double sample(double t)
+        {
+            double a = 0;
+            double b = 0;
+            double p = dutycycle * 2.0 * pi;
+
+            for (double n = 1; n < harmonics; n++)
+            {
+                double c = n * frequency * 2.0 * pi * t;
+                double c2 = Sinus(c);
+                a += -Sinus(c) / n;
+                b += -Sinus(c - p * n) / n;
+            }
+
+            return (2.0 * amplitude / pi) * (a - b);
+        }
+
+        static double Sinus(double t)
+        {
+            double j = t * 0.15915;
+            j = j - (int)j;
+            return 20.785 * j * (j - 0.5) * (j - 1.0f);
+        }
+    }
+
+    class sweeper { }
 }
