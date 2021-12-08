9a2c093189bf066b855e452669347002474e5ce0
Lennard Snapper <l.a.snapper@students.uu.nl>
2021-12-08 21:23:45 +0100

    added read and write extended NRom

---
+++ NesSharp/Mappers/BaseMapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NesSharp.Mappers
+{
+    abstract class BaseMapper
+    {
+        protected byte[] prgROM;
+        protected byte[] prgRAM;
+        protected byte[] chrROM;
+
+        public void SetPRGRom(byte[] PRGDump)
+        {
+            for (int i = 0; i < PRGDump.Length; i++)
+            {
+                prgROM[i] = PRGDump[i];
+            }
+        }
+
+        public void SetCHRRom(byte[] CHRDump)
+        {
+            for (int i = 0; i < CHRDump.Length; i++)
+            {
+                chrROM[i] = CHRDump[i];
+            }
+        }
+
+        public abstract byte ReadPrgROM(ushort address);
+        public abstract byte ReadPrgRAM(ushort address);
+        public abstract byte ReadChrROM(ushort address);
+
+        public abstract void WritePrgROM(ushort address, byte data);
+        public abstract void WritePrgRAM(ushort address, byte data);
+        public abstract void WriteChrROM(ushort address, byte data);
+    }
+
+
+}
---
+++ NesSharp/Mappers/NRom.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NesSharp.Mappers
+{
+    class NRom : BaseMapper
+    {
+        public NRom()
+        {
+            this.prgROM = new byte[0x8000];
+            this.prgRAM = new byte[0x2000];
+            this.chrROM = new byte[0x2000];
+        }
+
+        public override byte ReadChrROM(ushort address)
+        {
+            return chrROM[address];
+        }
+
+        public override byte ReadPrgRAM(ushort address)
+        {
+            return prgRAM[address];
+        }
+
+        public override byte ReadPrgROM(ushort address)
+        {
+            return prgROM[address];
+        }
+
+        public override void WriteChrROM(ushort address, byte data)
+        {
+            
+        }
+
+        public override void WritePrgRAM(ushort address, byte data)
+        {
+            prgRAM[address] = data;
+        }
+
+        public override void WritePrgROM(ushort address, byte data)
+        {
+            
+        }
+    }
+}
