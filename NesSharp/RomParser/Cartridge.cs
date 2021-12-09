d675f6faa6db908962acb8bf34761260340204e6
Sem de Haan <s.f.dehaan1@students.uu.nl>
2021-12-09 13:33:43 +0100

    Coded RomParserTests

--- NesSharp/RomParser/Cartridge.cs
+++ NesSharp/RomParser/Cartridge.cs
@@ -2,12 +2,28 @@
 
 namespace NesSharp 
 {
-	public enum MirrorType {vertical, horizontal}
+	public enum MirrorType {vertical, horizontal};
+	public enum ConsoleType {NES, VSYS, PC10, ECT};
+	public enum TimingType {NTSC, PAL, MR, DENDY};
+
+	public class VsysSpecifics
+    {
+		public int ppuType, hardwareType;
+    }
+
+	public class EctSpecifics
+    {
+		public int consoleType;
+    }
 
 	public class Cartridge
 	{
-		public int rombanks, vrombanks, mapperType, rambanks;
-		public bool batteryRam, trainer, fourScreen, vsystem, pal;
+		public int rombanks, vrombanks, rambanks, mapperType, submapper, prgramSize, eepromSize, chrramSize, chrnvramSize, misrom, expansionDevice;
+		public bool batteryRam, trainer, fourScreen;
+		public byte[] trainerbytes, rombytes, vrombytes, misrombytes;
+		public object consoleTypeSpecifics;
 		public MirrorType mirroring;
+		public ConsoleType consoleType;
+		public TimingType timingType;
 	}
 }
