b81f6840a9ca7a803db63e2b909157a2df407310
Astavie <astavie@protonmail.com>
2021-12-15 14:40:54 +0100

    Fix ppu tests

--- NesSharpTests/PPUTests.cs
+++ NesSharpTests/PPUTests.cs
@@ -42,8 +42,8 @@ namespace NesSharpTests {
 
         public void ReadOutput() {
             ushort read = 0x6004;
-            while (ram.Read(read) != 0 && read < 0x8000) {
-                Console.Write((char) ram.Read(read));
+            while (ram.Read(read).Item1 != 0 && read < 0x8000) {
+                Console.Write((char) ram.Read(read).Item1);
                 read += 1;
             }
         }
@@ -58,10 +58,10 @@ namespace NesSharpTests {
                 bus.Tick();
                 bus.Tick();
                 /* Console.WriteLine(cpu.DumpCycle()); */
-                if (!started && ram.Read(0x6000) == 0x80) {
+                if (!started && ram.Read(0x6000).Item1 == 0x80) {
                     started = true;
                 }
-                else if (started && ram.Read(0x6000) != 0x80) {
+                else if (started && ram.Read(0x6000).Item1 != 0x80) {
                     break;
                 }
                 if (cpu.instr.Illegal) {
@@ -88,70 +88,70 @@ namespace NesSharpTests {
         public void VblBasics() {
             ReadNES("01-vbl_basics.nes");
             Run();
-            Assert.AreEqual(0, ram.Read(0x6000));
+            Assert.AreEqual(0, ram.Read(0x6000).Item1);
         }
 
         [Test]
         public void VblSetTime() {
             ReadNES("02-vbl_set_time.nes");
             Run();
-            Assert.AreEqual(0, ram.Read(0x6000));
+            Assert.AreEqual(0, ram.Read(0x6000).Item1);
         }
         
         [Test]
         public void VblClearTime() {
             ReadNES("03-vbl_clear_time.nes");
             Run();
-            Assert.AreEqual(0, ram.Read(0x6000));
+            Assert.AreEqual(0, ram.Read(0x6000).Item1);
         }
         
         [Test]
         public void NmiControl() {
             ReadNES("04-nmi_control.nes");
             Run();
-            Assert.AreEqual(0, ram.Read(0x6000));
+            Assert.AreEqual(0, ram.Read(0x6000).Item1);
         }
         
         [Test]
         public void NmiTiming() {
             ReadNES("05-nmi_timing.nes");
             Run();
-            Assert.AreEqual(0, ram.Read(0x6000));
+            Assert.AreEqual(0, ram.Read(0x6000).Item1);
         }
         
         [Test]
         public void Suppression() {
             ReadNES("06-suppression.nes");
             Run();
-            Assert.AreEqual(0, ram.Read(0x6000));
+            Assert.AreEqual(0, ram.Read(0x6000).Item1);
         }
         
         [Test]
         public void NmiOnTiming() {
             ReadNES("07-nmi_on_timing.nes");
             Run();
-            Assert.AreEqual(0, ram.Read(0x6000));
+            Assert.AreEqual(0, ram.Read(0x6000).Item1);
         }
         
         [Test]
         public void NmiOffTiming() {
             ReadNES("08-nmi_off_timing.nes");
             Run();
-            Assert.AreEqual(0, ram.Read(0x6000));
+            Assert.AreEqual(0, ram.Read(0x6000).Item1);
         }
         
         [Test]
         public void EvenOddFrames() {
             ReadNES("09-even_odd_frames.nes");
             Run();
-            Assert.AreEqual(0, ram.Read(0x6000));
+            Assert.AreEqual(0, ram.Read(0x6000).Item1);
         }
         
         [Test]
         public void EvenOddTiming() {
             ReadNES("10-even_odd_timing.nes");
             Run();
-            Assert.AreEqual(0, ram.Read(0x6000));
+            Assert.AreEqual(0, ram.Read(0x6000).Item1);
         }
     }
 }
